57685b8355329db57d4ffc124117624669ea84a3
Prosteel Group <prosteelgroup@MacBook-Air-von-Prosteel-4.local>
2026-09-07 08:55:54 +0300

    Use compatible Go network module

--- tests/test_backup_restore_contract.py
+++ tests/test_backup_restore_contract.py
@@ -67,7 +67,7 @@ def test_backup_image_uses_pinned_tools_and_non_root_runtime() -> None:
     assert "ARG RESTIC_SOURCE_SHA256=" in dockerfile
     for module in (
         "golang.org/x/crypto@v0.56.0",
-        "golang.org/x/net@v0.56.0",
+        "golang.org/x/net@v0.57.0",
         "golang.org/x/text@v0.39.0",
         "google.golang.org/grpc@v1.83.1",
     ):
